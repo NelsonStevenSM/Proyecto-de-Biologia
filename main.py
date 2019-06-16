bb9b85236396d0a4bbdc9d793e0c826c4b428176
NelsonStevenSM <nsanabiom@uni.pe>
2019-06-16 13:28:26 -0500

    modificado

--- main.py
+++ main.py
@@ -1,63 +1,18 @@
 from PyQt5 import QtGui, QtCore
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
+from PyQt5.QtCore import Qt
 import os.path
-class configButton(QWidget):
-    def __init__(self,parent, path, acro):
-        super().__init__(parent)
-
-        self.info = os.path.basename(path).split('.')
-        print(self.info)
-       
-        self.but = QPushButton(self)
-        self.but.setIcon(QtGui.QIcon(QPixmap(path)))
-        self.but.clicked.connect(self.handleButton)
-        self.but.setIconSize(QtCore.QSize(50, 50))
-
-        self.lbl = QLabel(acro)
-        self.lbl.setAlignment(QtCore.Qt.AlignCenter)
-        self.lbl.setFont(QtGui.QFont("Arial", 12, QtGui.QFont.Bold))
-
-        self.layoutConf = QVBoxLayout(self)
-
-        self.layoutConf.addWidget(self.but)
-        self.layoutConf.addWidget(self.lbl)
-
-        self.setLayout (self.layoutConf)
-
-
-    def handleButton(self):
-        pass
-
-class Buscador(QWidget):
-
-    def __init__(self,parent):
-        super(Buscador, self).__init__(parent)
-        
-        self.layout = QHBoxLayout(self)
-
-        self.textB = QLineEdit(self)
-        self.textB.setPlaceholderText("Buscar por Familia ... ")   
-
-        self.layout.addWidget(self.textB)
-
-        self.search = QPushButton("Buscar")
-
-        self.layout.addWidget (self.search)
-        self.setLayout (self.layout)
-
-
-
+from tab1 import *
 class Window(QMainWindow):
-    def __init__(self, parent = None):
-        super(Window, self).__init__(parent)
+    def __init__(self):
+        super(Window, self).__init__()
 
         self.title = "FiloPlant"
         self.top = 100
         self.left = 100
-        self.width = 680
-        self.height = 500
-        self.loadImage()
+        self.width = 500
+        self.height = 300
         self.InitWindow()
 
     def InitWindow(self):
@@ -66,59 +21,73 @@ class Window(QMainWindow):
         self.setWindowTitle(self.title)
         self.setGeometry(self.top, self.left, self.width, self.height)
 
-        self.map = QPushButton("Mapa")
-
-        grid = QGridLayout()
-        grid.setSpacing(7)
-        grid.addWidget(self.button00,0,0)
-        grid.addWidget(self.button01,0,1)
-        grid.addWidget(self.button10,1,0)
-        grid.addWidget(self.button11,1,1)
-        grid.addWidget(self.button20,2,0)
-        grid.addWidget(self.button21,2,1)
-        grid.addWidget(self.map,3,0,1,2)
         # Creando Layot Padre 
         wid = QWidget(self)
 
         self.setCentralWidget(wid)
 
-        self.form = Buscador(self)
-
         # Layout
-        LGeneral = QHBoxLayout()
+        LGeneral = QVBoxLayout()
         LDescriptor = QVBoxLayout()
-        LGyForm = QVBoxLayout()
 
         # Filoplant
         Descrip = QLabel(self)
-        Descrip.setPixmap(QPixmap('./img/filoplant.jpeg'))
-        
+        Descrip.setPixmap(QPixmap('./img/filoplant.png'))
+        Descrip.setAlignment(QtCore.Qt.AlignCenter) 
+
+        bizagi = QLabel(self)
+        bizagi.setPixmap(QPixmap('./img/logo.png'))
+        bizagi.setAlignment(QtCore.Qt.AlignCenter) 
+
+  #      self.endemica = QLabel("Buscar por región endémica")
+   #     self.endemica.setFont(QtGui.QFont("Arial Black", 15, QtGui.QFont.Bold))
+    #self.endemica.setAlignment(QtCore.Qt.AlignCenter)
+
+        self.config = QLabel("", self)
+        self.config.move(20, 260)
+        self.config.setStyleSheet("background-color: transparent;")
+        self.config.resize(90, 90)
+        self.config.setCursor(QCursor(Qt.PointingHandCursor))
+        self.config.mousePressEvent = self.clickConfig
+
+        self.choose = QLabel("", self)
+        self.choose.move(292, 310)
+        self.choose.setStyleSheet("background-color: transparent;")
+        self.choose.resize(90, 90)
+        self.choose.setCursor(QCursor(Qt.PointingHandCursor))
+#        self.choose.mousePressEvent = self.clickChoose
+
+        self.tree = QLabel("", self)
+        self.tree.move(553, 255)
+        self.tree.setStyleSheet("background-color: transparent;")
+        self.tree.resize(90, 90)
+        self.tree.setCursor(QCursor(Qt.PointingHandCursor))
+#        self.tree.mousePressEvent = self.clickTree
+
+        self.help = QLabel("", self)
+        self.help.move(403, 182)
+        self.help.setStyleSheet("background-color: transparent;")
+        self.help.resize(60, 60)
+        self.help.setCursor(QCursor(Qt.PointingHandCursor))
+#        self.help.mousePressEvent = self.clickHelp
+
+        self.map = QLabel("", self)
+        self.map.move(215, 179)
+        self.map.setStyleSheet("background-color: transparent;")
+        self.map.resize(60, 60)
+        self.map.setCursor(QCursor(Qt.PointingHandCursor))
+#        self.map.mousePressEvent = self.clickMap
+           
         LDescriptor.addWidget(Descrip)
-
-        LGeneral.addLayout(LDescriptor,1)
-
-        self.endemica = QLabel("Buscar por región endémica")
-        self.endemica.setFont(QtGui.QFont("Arial Black", 15, QtGui.QFont.Bold))
-        self.endemica.setAlignment(QtCore.Qt.AlignCenter)
-
-        LGyForm.addWidget(self.endemica)
-
-        LGyForm.addLayout(grid)
-        LGyForm.addWidget(self.form)
-        LGeneral.addLayout(LGyForm,1)
-
+        LDescriptor.addWidget(bizagi)
+        LGeneral.addLayout(LDescriptor)
         wid.setLayout(LGeneral)
 
         self.show()
 
-    def loadImage(self):
-
-        self.button00 = configButton(self,"./img/Epidendrum_secundum.jpg", "BMHP")
-        self.button01 = configButton(self,"./img/Masdevallia_barlaeana.jpg", "AA")
-        self.button10 = configButton(self,"./img/Masdevallia_urosalpinx.jpg", "BHA")
-        self.button11 = configButton(self,"./img/Masdevallia_davisii.jpg", "BS")
-        self.button20 = configButton(self,"./img/Masdevallia_uniflora.jpg", "MDE")
-        self.button21 = configButton(self,"./img/Phragmipedium.jpg", "PHS")
+    def clickConfig(self, event):
+        Alineamiento()
+        print("nelson")
 
 if __name__ =="__main__":
 
